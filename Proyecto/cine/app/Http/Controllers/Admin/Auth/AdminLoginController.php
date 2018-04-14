f503de17c895c48590df7e3b6df5af265111dbaf
Lorena <tranity06@gmail.com>
2018-04-14 18:18:40 +0200

    todo ok

--- Proyecto/cine/app/Http/Controllers/Admin/Auth/AdminLoginController.php
+++ Proyecto/cine/app/Http/Controllers/Admin/Auth/AdminLoginController.php
@@ -31,7 +31,7 @@ class AdminLoginController extends Controller
 
     public function showLoginForm()
     {
-      return view('auth.login');
+      return view('admin.login');
     }
 
     public function login(Request $request)
@@ -42,11 +42,11 @@ class AdminLoginController extends Controller
         'email'   => 'required|email',
         'password' => 'required|min:6'
       ]);
-
+      
       // Attempt to log the user in
       if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
-        // if successful, then redirect to their intended location
-        return redirect()->intended(route('home'));
+        // if successful, then redirect to their intended location 
+        return redirect()->intended(route('login'));
       }
 
       // if unsuccessful, then redirect back to the login with the form data
--- Proyecto/cine/app/Http/Controllers/Admin/Home.php
+++ Proyecto/cine/app/Http/Controllers/Admin/Home.php
@@ -25,7 +25,7 @@ class Home extends Controller
      */
     public function index()
     { 
-        return view('home');
+        return view('admin.home');
     }
 
     public function logout(Request $request)
