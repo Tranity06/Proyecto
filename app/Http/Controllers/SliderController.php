cb6d4b15bba5a491498a5210d2ca2771061bc296
Emanuel <aemanuels123@gmail.com>
2018-05-23 20:49:10 +0200

    update: eliminados ficheros que no se utilizan.

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pelicula;
-use App\Models\Resena;
-use App\Models\Sesion;
 
 class SliderController extends Controller
 {
