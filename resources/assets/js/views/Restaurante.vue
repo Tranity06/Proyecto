46c3e3f5fce9b438cc77f74f3665f214e2ef69ee
Emanuel <aemanuels123@gmail.com>
2018-05-27 16:42:19 +0200

    add: Restaurante diseño

--- resources/assets/js/views/Restaurante.vue
+++ resources/assets/js/views/Restaurante.vue
@@ -1,8 +1,52 @@
 <template>
-    <div class="section">
-        <div class="container">
-            <div class="columns is-mobile is-multiline">
-                <div class="column is-half-mobile is-one-quarter-desktop">
+    <div>
+        <section class="hero is-dark">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Hero title
+                    </h1>
+                    <h2 class="subtitle">
+                        Hero subtitle
+                    </h2>
+                </div>
+            </div>
+            <div class="hero-foot">
+                <nav class="tabs is-boxed is-fullwidth">
+                    <div class="container">
+                        <ul>
+                            <li class="is-active">
+                                <a>
+                                    <span class="icon is-small"><i class="fas fa-cog" aria-hidden="true"></i></span>
+                                    <span class="is-hidden-mobile">CATEGORIA</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="icon is-small"><i class="fas fa-ticket-alt" aria-hidden="true"></i></span>
+                                    <span class="is-hidden-mobile">CATEGORIA</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="icon is-small"><i class="far fa-comment" aria-hidden="true"></i></span>
+                                    <span class="is-hidden-mobile">CATEGORIA</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span class="icon is-small"><i class="far fa-star" aria-hidden="true"></i></span>
+                                    <span class="is-hidden-mobile">CATEGORIA</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </section>
+        <div class="section">
+            <div class="container">
+                <div class="flexcontainer">
                     <div class="tarjeta">
                         <div class="titulo is-size-5">
                             Nombre
@@ -13,47 +57,63 @@
                             <div class="alergenos">
                                 iconos al..
                             </div>
-                            <div class="comprar">
-                               comprar
-                            </div>
+                            <button class="button is-warning is-rounded is-small">Comprar</button>
                         </div>
                     </div>
-                </div>
-                <div class="column is-half-mobile is-one-quarter-desktop">
                     <div class="tarjeta">
                         <div class="titulo is-size-5">
-                            Hola
+                            Nombre
                         </div>
                         <div class="imagen">
                         </div>
                         <div class="botones-debajo">
                             <div class="alergenos">
-                                A B C
+                                iconos al..
                             </div>
-                            <div class="comprar">
-                                A
-                            </div>
+                            <button class="button is-warning is-rounded is-small">Comprar</button>
                         </div>
                     </div>
-                </div>
-                <div class="column is-half-mobile is-one-quarter-desktop">
                     <div class="tarjeta">
                         <div class="titulo is-size-5">
-                            Hola
+                            Nombre
                         </div>
                         <div class="imagen">
                         </div>
                         <div class="botones-debajo">
                             <div class="alergenos">
-                                A B C
+                                iconos al..
                             </div>
-                            <div class="comprar">
-                                A
-                            </div>
+                            <button class="button is-warning is-rounded is-small">Comprar</button>
                         </div>
+                    </div>
+                    <div class="tarjeta">
+                        <div class="titulo is-size-5">
+                            Nombre
+                        </div>
+                        <div class="imagen">
+                        </div>
+                        <div class="botones-debajo">
+                            <div class="alergenos">
+                                iconos al..
+                            </div>
+                            <button class="button is-warning is-rounded is-small">Comprar</button>
+                        </div>
+                    </div>
+                    <div class="tarjeta">
+                        <div class="titulo is-size-5">
+                            Nombre
+                        </div>
+                        <div class="imagen">
+                        </div>
+                        <div class="botones-debajo">
+                            <div class="alergenos">
+                                iconos al..
+                            </div>
+                            <button class="button is-warning is-rounded is-small">Comprar</button>
                     </div>
                 </div>
 
+                </div>
             </div>
         </div>
     </div>
@@ -66,6 +126,27 @@
 </script>
 
 <style scoped>
+
+
+    .flexcontainer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* You can set flex-wrap and
+           flex-direction individually */
+        flex-direction: row;
+        flex-wrap: wrap;
+        /* Or do it all in one line
+          with flex flow */
+        flex-flow: row wrap;
+        /* tweak where items line
+           up on the row
+           valid values are: flex-start,
+           flex-end, space-between,
+           space-around, stretch */
+        align-content: flex-end;
+    }
+
     .tarjeta{
         display: flex;
         flex-direction: column;
@@ -73,10 +154,16 @@
 
         width: 250px;
         height: auto;
+        margin: .5rem .5rem;
 
         box-shadow: 0 0 25px rgba(0,0,0,0.08);
         background-color: #fff;
         border-radius: 8px;
+        transition: box-shadow 0.5s;
+    }
+
+    .tarjeta:hover {
+        box-shadow: 0 0 25px rgba(0,0,0,0.18);
         transition: box-shadow 0.5s;
     }
 
