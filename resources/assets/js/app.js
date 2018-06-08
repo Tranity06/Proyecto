09d0272bf3b89f4d30ed795784c1286a0be7a962
Emanuel <aemanuels123@gmail.com>
2018-06-08 21:31:03 +0200

    Configuración Web

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -35,7 +35,7 @@ Vue.use(AlgoliaComponents);
 moment.locale('es');
 
 window.axios = axios;
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://palomitastime.online';
 
 /*
 // before a request is made start the nprogress
